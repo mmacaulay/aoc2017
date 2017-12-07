433a823630a9f367d8d7c137d1d938e51981e545
Matt MacAulay <mac@twg.io>
2017-12-06 23:50:36 -0500

    Day 4 part 2

--- day4.scm
+++ day4.scm
@@ -14,20 +14,38 @@
          ]))
 
 (define (isvalid str)
-    (display (string-join (list str ":" (format "~a" (nodupes? (string-split str) (list->set '())))) " "))
+    (display (string-join (list str ":" (format "~a" (nodupes? (string-split str) (set '())))) " "))
     (newline))
 
 (define (countdupes lines)
-  (sequence-count (lambda(line) (nodupes? (string-split line) (list->set '()))) lines))
+  (sequence-count (lambda(line) (nodupes? (string-split line) (set '()))) lines))
 
 (define (testdata)
   (isvalid "aa bb cc dd ee")
   (isvalid "aa bb cc dd aa")
   (isvalid "aa bb cc dd aaa"))
 
+(define (countanagrams lines)
+  (sequence-count (lambda(line) (noanagrams? (string-split line))) lines))
+
+(define (combomatch? combo)
+  (string=? (first combo) (second combo)))
+
+(define (noanagrams? wordlist)
+  (match (findf (lambda(combo)
+                  (combomatch? (map (lambda(word)
+                                  (list->string
+                                    (sort (string->list word) char<?)))
+                                    combo)))
+           (combinations wordlist 2))
+         [#f #t]
+         [_ #f])
+)
+
 (define (day4)
   (define in (open-input-file "day4.txt"))
-  (display (countdupes (in-lines in)))
+  ; (display (countdupes (in-lines in)))
+  (display (countanagrams (in-lines in)))
   (close-input-port in))
 
 ; (testdata)
