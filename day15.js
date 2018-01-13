1963afdb889b13262ca11b97103a05401582b22f
Matt MacAulay <mac@twg.io>
2018-01-13 09:20:31 -0500

    Day 15 part 1

---
+++ day15.js
@@ -0,0 +1,62 @@
+
+class Generator {
+  constructor (startValue, factor) {
+    this.previousValue = this.startValue = startValue
+    this.factor = factor
+  }
+
+  reset () {
+    this.previousValue = this.startValue
+  }
+
+  nextValue () {
+    this.previousValue = (this.previousValue * this.factor) % 2147483647
+    return this.previousValue
+  }
+}
+
+class Judge {
+  constructor (a, b) {
+    this.a = a
+    this.b = b
+  }
+
+  countMatches (sampleSize) {
+    let count = 0
+    const mask = 65535
+
+    for (let i = 0; i < sampleSize; i++) {
+      const aval = this.a.nextValue()
+      const bval = this.b.nextValue()
+
+      if ((aval & mask) === (bval & mask)) {
+        count++
+      }
+    }
+    return count
+  }
+}
+
+function testData () {
+  const a = new Generator(65, 16807)
+  const b = new Generator(8921, 48271)
+
+  a.reset()
+  b.reset()
+
+  const j = new Judge(a, b)
+  const matchCount = j.countMatches(5)
+  console.log(`Test data ${matchCount}`)
+}
+
+function part1 () {
+  const a = new Generator(512, 16807)
+  const b = new Generator(191, 48271)
+
+  const j = new Judge(a, b)
+  const matchCount = j.countMatches(40000000)
+  console.log(`Part 1: ${matchCount}`)
+}
+
+testData()
+part1()
