c7be69a3689e2cc7668206c0184557959df1bf20
Matt MacAulay <mac@twg.io>
2018-01-26 10:03:15 -0500

    Day 18 part 2

--- day18.js
+++ day18.js
@@ -2,11 +2,20 @@ const fs = require('fs')
 const lines = fs.readFileSync('day18.txt', { encoding: 'utf8' }).split('\n')
 
 class Program {
-  constructor (instructions) {
+  constructor (instructions, id) {
     this.instructions = instructions
-    this.registers = {}
-    this.lastFrequency = 0
     this.instructionPointer = 0
+
+    this.registers = {}
+    if (typeof id !== 'undefined') {
+      this.registers['p'] = id
+      this.part2 = true
+      this.messageQueue = []
+      this.sendCount = 0
+      this.waiting = false
+      this.id = id
+    }
+    this.lastFrequency = 0
   }
 
   run () {
@@ -16,15 +25,29 @@ class Program {
     }
   }
 
+  next () {
+    if (this.isComplete()) return
+    this.execute(this.instructions[this.instructionPointer])
+  }
+
   execute (instruction) {
     const params = instruction.split(' ')
     const cmd = params[0]
     const register = params[1]
     const registerValue = this.read(register)
 
+    // console.log(`[${this.id}] - (${this.instructionPointer}) - ${instruction}`)
+
+    if (this.waiting) return
+
     switch (cmd) {
       case 'snd':
-        this.lastFrequency = registerValue
+        if (this.part2) {
+          this.other.receive(registerValue)
+          this.sendCount++
+        } else {
+          this.lastFrequency = registerValue
+        }
         break
       case 'set':
         this.registers[register] = this.readValue(params[2])
@@ -39,8 +62,17 @@ class Program {
         this.registers[register] %= this.readValue(params[2])
         break
       case 'rcv':
-        if (registerValue !== 0) {
-          return this.lastFrequency
+        if (this.part2) {
+          this.waitingRegister = register
+          if (this.messageQueue.length) {
+            this.handleRcv()
+          } else {
+            this.waiting = true
+          }
+        } else {
+          if (registerValue !== 0) {
+            return this.lastFrequency
+          }
         }
         break
       case 'jgz':
@@ -71,6 +103,21 @@ class Program {
       ? this.read(param)
       : int
   }
+
+  receive (value) {
+    this.messageQueue.push(value)
+    if (this.waiting) this.handleRcv()
+  }
+
+  handleRcv () {
+    this.registers[this.waitingRegister] = this.messageQueue.shift()
+    this.waiting = false
+    this.waitingRegister = null
+  }
+
+  isComplete () {
+    return this.instructionPointer >= this.instructions.length
+  }
 }
 
 function testData () {
@@ -95,5 +142,26 @@ function part1 () {
   console.log(`Part 1: ${program.run()}`)
 }
 
+function part2 () {
+  const program0 = new Program(lines, 0)
+  const program1 = new Program(lines, 1)
+
+  program0.other = program1
+  program1.other = program0
+
+  while (true) {
+    program0.next()
+    program1.next()
+
+    if ((program0.waiting && program1.waiting) || // deadlock
+      (program0.isComplete() && program1.isComplete())) { // both finished
+      break
+    }
+  }
+
+  console.log(`Part 2: ${program1.sendCount}`)
+}
+
 testData()
 part1()
+part2()
